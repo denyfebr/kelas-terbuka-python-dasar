0e8c4f95162d10484cbfaf4249b07a01bfea546b
Deny Febriyanto <denyfebri@gmail.com>
2024-09-01 22:17:12 +0700

    belajar python instalasi

--- 1_Instalasi/Main.py
+++ 1_Instalasi/Main.py
@@ -1,1 +1,15 @@
-print("hello world")
+import time
+start_time = time.time()
+print("Hello")
+print("World")
+print("Hello World")
+
+print("Halo Chantiiek")
+# ini adalah comment
+a = 10 # ini adalah comment juga
+"""Ini adalah comment
+dengan munggunakan multiline"""
+print(a)
+print(time.time() - start_time,"detik")
+# kita bisa mengcompile python ke bytecode
+# python -m py_compile Main.py
